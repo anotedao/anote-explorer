6c097839201d8aa8efecc0a9ab138271f576c315
Pragmatic <cryptopragmatic@protonmail.com>
2025-08-23 02:28:27 +0200

    rebranding and polishing up

--- src/js/Footer.js
+++ src/js/Footer.js
@@ -5,23 +5,20 @@ const socialLinks = [{
     url: 'https://github.com/anotedao/'
 }, {
     id: 'telegram',
-    url: 'https://t.me/AnoteDAO'
-}, {
-    id: 'facebook',
-    url: 'https://www.facebook.com/anotedao'
+    url: 'https://t.me/AintCoin'
 }];
 
 const Footer = ({version}) => {
     return (
         <div className="menu-footer">
             <div>Version: {version}</div>
-            <div>Brought to you by Anote Team</div>
+            <div>Brought to you by Aint Chain</div>
             <div>
                 {socialLinks.map(item =>
                     (<a key={item.id} className={`social ${item.id}`} href={item.url} target="_blank"></a>))}
             </div>
             <div>
-                <a className="fade" href="https://anotedao.com/" target="_blank">anotedao.com</a>
+                <a className="fade" href="https://aintchain.com/" target="_blank">aintchain.com</a>
             </div>
         </div>
     );
